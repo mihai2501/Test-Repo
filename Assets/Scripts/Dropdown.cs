76e3fbb6ca739d9ef566affc4f7a64b47863a9e2
mihai2501 <mihai2501@gmail.com>
2020-05-08 17:22:40 +0300

    added dropdown with shape selection

---
+++ Assets/Scripts/Dropdown.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Dropdown : MonoBehaviour
+{
+    public Vector3 spawnLocation;
+    public GameObject shapeHandler;
+    public GameObject hexagon;
+    public GameObject square;
+    public GameObject circle;
+    public GameObject triangle;
+
+    
+    public void HandleInputData(int val)
+    {
+        if (val == 0 )
+        {
+            Clear();
+            Instantiate(hexagon, shapeHandler.transform);
+        }
+        if (val==1)
+        {
+            Clear();
+            Instantiate(square, shapeHandler.transform);
+        }
+        if (val == 2)
+        {
+            Clear();
+            Instantiate(circle, shapeHandler.transform);
+        }
+        if (val == 3)
+        {
+            Clear();
+            Instantiate(triangle, shapeHandler.transform); 
+        }
+    }
+    public void Clear()
+    {
+        foreach (Transform child in shapeHandler.transform)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+    }
+}
